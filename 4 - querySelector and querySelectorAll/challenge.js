f436dc88a1e09b8c69890873c8613813f8941c38
Fluffy-Lion <leoncooper31@gmail.com>
2022-01-25 17:04:31 +0000

    colours

--- 4 - querySelector and querySelectorAll/challenge.js
+++ 4 - querySelector and querySelectorAll/challenge.js
@@ -1,6 +1,19 @@
-const listItems = document.querySelectorAll("li:nth-child(even)")
+const listItems = document.querySelectorAll("li")
 
+const colours = [
+    "red", 
+    "yellow", 
+    "goldenrod", 
+    "lightgreen", 
+    "blue", 
+    "purple"
+]
+
+let x = 0
 for(let i = 0; i < listItems.length; i++){
-    listItems[i].style.color = "lightgreen"
+    listItems[i].style.color = colours[x]
+    x++
+    if (x == colours.length){
+        x = 0
+    }
 }
-console.log(listItems)
