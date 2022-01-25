8a6c3a50ab9a0d46f2cad3659362f00a49954488
Fluffy-Lion <leoncooper31@gmail.com>
2022-01-25 15:52:33 +0000

    3rd challenge

--- 3 - Selecting Elements of Particular Type/app.js
+++ 3 - Selecting Elements of Particular Type/app.js
@@ -1,3 +1,24 @@
 const letters = document.getElementsByTagName("li")
+const notOrange = document.getElementsByClassName("not-orange")
 
-console.log(letters)
+// for(let i = 0; i < letters.length; i++){
+//     letters[i].style.color = "orange"
+// }
+
+// for(let i = 0; i < notOrange.length; i++) {
+//     notOrange[i].style.color = "red"
+// }
+
+// for(let i = 0; i < letters.length; i++){
+//     if(letters[i].className == "not-orange"){
+//         letters[i].style.color = "blue"
+//     } else {
+//         letters[i].style.color = "orange"
+//     }
+// }
+
+for(let i = 0; i < letters.length; i++){
+    if(letters[i].className != "not-orange"){
+        letters[i].style.color = "orange"
+    }
+}
